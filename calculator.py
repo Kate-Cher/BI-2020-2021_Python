0dbb8e79bb8254a55d9bc4e9827733d14765571b
Kate-Cher <kate-chernjavskaja@yandex.ru>
2020-09-10 22:44:13 +0300

    Added new functions

--- calculator.py
+++ calculator.py
@@ -1,7 +1,38 @@
-a = int(input())
-b = input()
-c = int(input())
-if b == "+": print(a+c)
-if b == "-": print(a-c)
-if b == "/": print(a/c)
-if b == "*": print(a*c)
+def check_isdigit(n):
+    f = 1
+    try:
+        float(n)
+    except ValueError:
+        f = 0
+    while f == 0:
+        n = input("Not a number. Enter a number: ")
+        try:
+            float(n)
+            f = 1
+        except ValueError:
+            f = 0
+    return float(n)
+
+fin = ''
+th = {'/', '*', '-', '+'}
+
+while fin != '!':
+    a = input("Enter a number: ")
+    a = check_isdigit(a)
+
+    b = input("Enter an operand: ")
+    while b not in th:
+        b = input("Choose operand from /, *, -, + and write it")
+
+    c = input("Enter a number: ")
+    c = check_isdigit(c)
+
+    if b == "+": print(a+c)
+    elif b == "-": print(a-c)
+    elif b == "/": print(a/c)
+    else: print(a*c)
+
+    print("Enter ! to finish or any key to continue")
+    fin = input()
+
+
